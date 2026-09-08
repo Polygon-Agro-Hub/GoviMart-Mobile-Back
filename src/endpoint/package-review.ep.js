2172b6eeb03e7db96248110122f00a8b9121bf80
Ashen Deelaka <122264429+ashendeelaka@users.noreply.github.com>
2026-09-08 11:45:03 +0530

    enhanced review pacakge dao

--- src/endpoint/package-review.ep.js
+++ src/endpoint/package-review.ep.js
@@ -114,7 +114,7 @@ exports.resetPackageItem = asyncHandler(async (req, res) => {
  */
 exports.confirmPackageReview = asyncHandler(async (req, res) => {
     const { userId } = req.user;
-    const { orderId, processOrderId, lockNow = false, additionalAmount = 0, replacements = [], additionalItems = [] } = req.body;
+    const { orderId, processOrderId, lockNow = false, additionalAmount = 0, newScheduleDate = null, replacements = [], additionalItems = [] } = req.body;
 
     console.log("[confirmPackageReview Endpoint] Received request by userId:", userId, "body:", JSON.stringify(req.body, null, 2));
 
@@ -132,6 +132,7 @@ exports.confirmPackageReview = asyncHandler(async (req, res) => {
             userId,
             lockNow,
             additionalAmount,
+            newScheduleDate,
             replacements,
             additionalItems,
         });
